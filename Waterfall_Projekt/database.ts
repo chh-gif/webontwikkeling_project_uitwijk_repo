e7fa76304f5615c90f12ff441c47815311476375
Chantal Höller-Holtrichter <chantal.holler-holtrichterstudent.ap.be>
2026-04-24 07:43:38 +0000

    CRUD angepasst

---
+++ Waterfall_Projekt/database.ts
@@ -0,0 +1,72 @@
+import { Collection, MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import { IntClimate, Waterfall } from "./interface";
+
+dotenv.config();
+
+export const client = new MongoClient(
+  process.env.MONGODB_URI || "mongodb://localhost:27017",
+);
+
+export const waterfalls: Collection<Waterfall> = client
+  .db("waterfallDB")
+  .collection<Waterfall>("waterfalls");
+export const climate: Collection<IntClimate> = client
+  .db("waterfallDB")
+  .collection<IntClimate>("climate");
+
+export async function connect() {
+  try {
+    await client.connect();
+    await openDB();
+    process.on("SIGINT", exit);
+  } catch (error) {
+    console.error(error);
+  }
+} // DB verbinden
+export async function fetchfunction() {
+  //nur fetchen wenn
+  const waterfallsArray: Waterfall[] = await openDB();
+  const climateArray: IntClimate[] = await openDBclimate();
+  if (waterfallsArray.length == 0) {
+    const resp1 = await fetch(
+      "https://raw.githubusercontent.com/chh-gif/Webontwikkeling-Assets_JSON_Rep/main/json/waterval.json",
+    );
+    const data = (await resp1.json()) as Waterfall[];
+    await waterfalls.deleteMany({});
+    await waterfalls.insertMany(data); //geen dubbel data
+  }
+
+  if (climateArray.length == 0) {
+    const resp1 = await fetch(
+      "https://raw.githubusercontent.com/chh-gif/Webontwikkeling-Assets_JSON_Rep/main/json/waterval_object.json",
+    );
+    const data = (await resp1.json()) as IntClimate[];
+    await climate.deleteMany({}); //geen dubbel data
+    await climate.insertMany(data);
+  }
+}
+export async function openDB() {
+  //DB öffnen
+  return await waterfalls.find({}).toArray();
+}
+
+export async function openDBclimate() {
+  //DB öffnen
+  return await climate.find({}).toArray();
+}
+export async function exit() {
+  try {
+    await client.close();
+  } catch (error) {
+    console.error(error);
+  }
+  process.exit(0);
+}
+
+export async function getAllWaterfalls() {
+  return await waterfalls.find({}).toArray();
+}
+export async function getWaterfallById(id: string) {
+  return await waterfalls.findOne({ waterfallId: id});
+}
--- Waterfall_Projekt/index.ts
+++ Waterfall_Projekt/index.ts
@@ -2,18 +2,24 @@ import express, { Express } from "express";
 import path from "path";
 import { IntClimate, Waterfall } from "./interface";
 import * as readline from "readline-sync";
-import {data, data2} from "./functions";
-import {ViewAllData} from "./functions";
-import {FilterById} from "./functions";
-import {MongoClient} from "mongodb";
-  
-const app : Express = express();
+import { ViewAllData } from "./functions";
+import { FilterById } from "./functions";
+import {
+  connect,
+  openDB,
+  fetchfunction,
+  getAllWaterfalls,
+  getWaterfallById,
+} from "./database";
+import { waterfalls, climate } from "./database";
+
+const app: Express = express();
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-app.set('views', path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("port", process.env.PORT || 3000);
 
 let waterfallObject: Waterfall[] = [];
@@ -22,10 +28,9 @@ let db: any;
 
 let choice = "";
 
-
 export async function main() {
-  await data(waterfallObject);
-  await data2(climateObject);
+  //await data(waterfallObject);
+  //await data2(climateObject);
   do {
     console.log("Welcome to the JSON data viewer");
     console.log("1. View all data");
@@ -51,132 +56,139 @@ export async function main() {
 
 //main();
 
-
 (async () => {
-  try{
-        
-  // Daten aus Funktionen holen
-  await data(waterfallObject);
-  await data2(climateObject);
+  try {
+    //MongoDB verbinden
+    connect();
+    // Daten aus Funktionen holen
 
-//MongoDB verbinden
-const url = "mongodb+srv://CHH:deweUTuxX4t7UngG@2webontwikkeling.cxz0skq.mongodb.net/";
-const client = new MongoClient(url);
-await client.connect();
-db = client.db("waterfallDB");
+    await fetchfunction();
 
- const find = await db.collection("waterfalls").findOne({});
-        if (find === null){
-//DB leeren
+    app.get("/", async (req, res) => {
+      const waterfall = await getAllWaterfalls();
+      res.render("index", { waterfallObject: waterfall });
+    });
 
-await db.collection("waterfalls").deleteMany({});
-await db.collection("climate").deleteMany({});
+    app.get("/overview", async (req, res) => {
+      const waterfall = await getAllWaterfalls();
+      res.render("overview", { waterfallObject: waterfall });
+    });
+    app.get("/detailpage/:id", async (req, res) => {
+      const id = req.params.id;
+      const allWaterfalls = await getAllWaterfalls();
+      const waterfall = await getWaterfallById(id);
 
-//DB füllen
+      res.render("detailpage", { element: waterfall, waterfallObject: allWaterfalls });
+    });
 
-await db.collection("waterfalls").insertMany(waterfallObject);
-await db.collection("climate").insertMany(climateObject);
-        }
-      
+    app.get("/climatepage/:id", async (req, res) => {
+      const id = req.params.id;
+      const waterfall = await getAllWaterfalls();
+      const element = await waterfalls.findOne(
+        (value: Waterfall) => value.climate.climateId === id,
+      );
+      res.render("climatepage", { element, waterfallObject: waterfall });
+    });
 
-app.get("/", async (req, res) =>{
-  const waterfalls = await db.collection("waterfalls").find().toArray();
-res.render("index", {waterfallObject: waterfalls})
-});
+    app.get("/climate", async (req, res) => {
+      const climateArray = await climate.find().toArray();
+      res.render("climate", { climateObject: climateArray });
+    });
 
-app.get("/overview", async (req, res) => {
-  const waterfalls = await db.collection("waterfalls").find().toArray();
-    res.render("overview", {waterfallObject: waterfalls})
-});
-app.get("/detailpage/:id", async (req, res) => {
-  const id = req.params.id;
-  const waterfalls = await db.collection("waterfalls").find().toArray();
-  const element = waterfalls.find((value: Waterfall) => value.waterfallId === id);
-    res.render("detailpage", {element, waterfallObject: waterfalls})
-});
+    app.post("/overview", async (req, res) => {
+      const search = typeof req.body.search == "string" ? req.body.search : "";
+      const waterfall = await getAllWaterfalls();
+      const result = waterfall.filter(
+        (value: Waterfall) =>
+          value.waterfallId.toLowerCase().includes(search.toLowerCase()) ||
+          value.country.toLowerCase().includes(search.toLowerCase()) ||
+          value.name.toLowerCase().includes(search.toLowerCase()) ||
+          value.type.toLowerCase().includes(search.toLowerCase()),
+      );
 
-app.get("/climatepage/:id", async(req, res) => {
-  const id = req.params.id;
- const waterfalls = await db.collection("waterfalls").find().toArray();
-  const element = waterfalls.find((value: Waterfall) => value.climate.climateId === id);
-    res.render("climatepage", {element, waterfallObject: waterfalls})
-});
+      const sort = req.body.sort;
+      const direction = req.body.direction;
 
-app.get("/climate", async(req, res) => {
-  const climate = await db.collection("climate").find().toArray();
-    res.render("climate", {climateObject: climate})
-});
-
-app.post("/overview", async(req, res)=>{
-const search = typeof req.body.search == "string"? req.body.search: "";
-const waterfalls = await db.collection("waterfalls").find().toArray();
-const result = waterfalls.filter((value: Waterfall)=> 
-  value.waterfallId.toLowerCase().includes(search.toLowerCase()) ||
-value.country.toLowerCase().includes(search.toLowerCase()) ||
-value.name.toLowerCase().includes(search.toLowerCase()) ||
-value.type.toLowerCase().includes(search.toLowerCase())
-);
-
-const sort = req.body.sort;
-const direction = req.body.direction;
-
-
-switch(sort){
-  case "name":
-     result.sort((a: Waterfall,b: Waterfall)=> direction == "asc"? a.name.localeCompare(b.name):b.name.localeCompare(a.name));
-  break;
-     case "country":
-    result.sort((a: Waterfall,b: Waterfall)=> direction =="asc"? a.country.localeCompare(b.country):b.country.localeCompare(a.country));
-    break;
-  case "height": 
-  result.sort((a: Waterfall,b: Waterfall)=>direction == "asc"? a.heightInM-b.heightInM:b.heightInM-a.heightInM);
-  break;
-  case "date": 
-  result.sort((a: Waterfall,b: Waterfall)=> direction == "asc"? new Date(a.dateOfFirstDocumentary).getTime()- new Date(b.dateOfFirstDocumentary).getTime(): new Date (b.dateOfFirstDocumentary).getTime()- new Date (a.dateOfFirstDocumentary).getTime());
-    break;
-  case "flow":
-    result.sort((a: Waterfall,b: Waterfall)=>direction == "asc"? Number(a.yearRoundWaterFlow)-Number(b.yearRoundWaterFlow):Number(b.yearRoundWaterFlow)-Number(a.yearRoundWaterFlow));
-   break;
-    case "type":
-result.sort((a: Waterfall,b: Waterfall)=> direction == "asc"? a.type.localeCompare(b.type):b.type.localeCompare(a.type));
-break;
-}
-res.render("overview", {waterfallObject: result})
-});
-
-app.get("/editor/:id", async(req,res) => {
-const id = req.params.id;
-const waterfalls = await db.collection("waterfalls").find().toArray();
-const element = waterfalls.find((value: Waterfall) => value.waterfallId === id);
-res.render("editor", {element, waterfallObject: waterfalls});
-}
-);
-
-app.post("/editor/:id", async (req, res) => {
-  const id = req.params.id;
-
-  await db.collection("waterfalls").updateOne(
-    { waterfallId: id },
-    {
-      $set: {
-        description: req.body.description,
-        yearRoundWaterFlow: req.body.yearRoundWaterFlow === "true"? true : false,
-        imageURL: req.body.imageURL,
-        imageSource: req.body.imageSource
+      switch (sort) {
+        case "name":
+          result.sort((a: Waterfall, b: Waterfall) =>
+            direction == "asc"
+              ? a.name.localeCompare(b.name)
+              : b.name.localeCompare(a.name),
+          );
+          break;
+        case "country":
+          result.sort((a: Waterfall, b: Waterfall) =>
+            direction == "asc"
+              ? a.country.localeCompare(b.country)
+              : b.country.localeCompare(a.country),
+          );
+          break;
+        case "height":
+          result.sort((a: Waterfall, b: Waterfall) =>
+            direction == "asc"
+              ? a.heightInM - b.heightInM
+              : b.heightInM - a.heightInM,
+          );
+          break;
+        case "date":
+          result.sort((a: Waterfall, b: Waterfall) =>
+            direction == "asc"
+              ? new Date(a.dateOfFirstDocumentary).getTime() -
+                new Date(b.dateOfFirstDocumentary).getTime()
+              : new Date(b.dateOfFirstDocumentary).getTime() -
+                new Date(a.dateOfFirstDocumentary).getTime(),
+          );
+          break;
+        case "flow":
+          result.sort((a: Waterfall, b: Waterfall) =>
+            direction == "asc"
+              ? Number(a.yearRoundWaterFlow) - Number(b.yearRoundWaterFlow)
+              : Number(b.yearRoundWaterFlow) - Number(a.yearRoundWaterFlow),
+          );
+          break;
+        case "type":
+          result.sort((a: Waterfall, b: Waterfall) =>
+            direction == "asc"
+              ? a.type.localeCompare(b.type)
+              : b.type.localeCompare(a.type),
+          );
+          break;
       }
-    }
-  );
+      res.render("overview", { waterfallObject: result });
+    });
 
-  res.redirect("/detailpage/" + id);
-});
+    app.get("/editor/:id", async (req, res) => {
+      const id = req.params.id;
+      const waterfall = await getAllWaterfalls();
+      const element = await getWaterfallById(id);
+      res.render("editor", { element, waterfallObject: waterfall });
+    });
 
-      }
-        catch (err) {
+    app.post("/editor/:id", async (req, res) => {
+      const id = req.params.id;
+
+      await waterfalls.updateOne(
+        { waterfallId: id },
+        {
+          $set: {
+            description: req.body.description,
+            yearRoundWaterFlow:
+              req.body.yearRoundWaterFlow === "true" ? true : false,
+            imageURL: req.body.imageURL,
+            imageSource: req.body.imageSource,
+          },
+        },
+      );
+
+      res.redirect("/detailpage/" + id);
+    });
+  } catch (err) {
     console.error(err);
   }
 
-  app.listen(app.get("port"), () => {
+  app.listen(app.get("port"), async () => {
+    await connect();
     console.log("Server started on http://localhost:" + app.get("port"));
   });
 })();
-
